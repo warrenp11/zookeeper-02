b93724f3c7a5eb9d9bd8f026bdd0589ebc21b606
Peter <warrenp11@gmail.com>
2021-11-04 21:22:14 -0400

    created a port using express, can now access animals.json data

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+const { animals } = require("./data/animals.json");
+
+app.get("/api/animals", (req, res) => {
+  // // access query property on the req object
+  //console.log(req.query);
+
+  let results = animals;
+  res.json(results);
+});
+
 app.listen(3001, () => {
   console.log(`API server now on port 3001`);
 });
