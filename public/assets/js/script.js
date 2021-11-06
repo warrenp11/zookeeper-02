3dc7b1b540f16fd88c1d6c1cd5325770f0aa0299
Peter <warrenp11@gmail.com>
2021-11-06 17:13:25 -0400

    added fetch api in  function in scripts.js file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
-const $animalForm = document.querySelector('#animal-form');
+const $animalForm = document.querySelector("#animal-form");
 
-const handleAnimalFormSubmit = event => {
+const handleAnimalFormSubmit = (event) => {
   event.preventDefault();
 
   // get animal data and organize it
@@ -16,16 +16,37 @@ const handleAnimalFormSubmit = event => {
   }
 
   if (diet === undefined) {
-    diet = '';
+    diet = "";
   }
 
-  const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions;
+  const selectedTraits = $animalForm.querySelector(
+    '[name="personality"'
+  ).selectedOptions;
   const personalityTraits = [];
   for (let i = 0; i < selectedTraits.length; i += 1) {
     personalityTraits.push(selectedTraits[i].value);
   }
   const animalObject = { name, species, diet, personalityTraits };
 
+  // use fetch api to post data
+  fetch("/api/animals", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(animalObject),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert("Error: " + response.statusText);
+    })
+    .then((postResponse) => {
+      console.log(postResponse);
+      alert("Thank you for adding an animal");
+    });
 };
 
-$animalForm.addEventListener('submit', handleAnimalFormSubmit);
+$animalForm.addEventListener("submit", handleAnimalFormSubmit);
