93aecfc795a51b182657a67ec25326aab9c9eb8b
Peter <warrenp11@gmail.com>
2021-11-09 08:11:17 -0500

    rerouted html paths

--- server.js
+++ server.js
@@ -10,22 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/animals.html"));
-});
-
-app.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
 });
