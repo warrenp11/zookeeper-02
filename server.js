d9455383903706fc4a8df22af26f1400a93abfd8
Peter <warrenp11@gmail.com>
2021-11-09 08:19:00 -0500

    directed express to use apiRoutes and htmlRoutes

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals.json");
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
