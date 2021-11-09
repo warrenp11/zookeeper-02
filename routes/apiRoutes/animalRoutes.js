7a3509398eb9d9e32302efb1152aab5d3a47adee
Peter <warrenp11@gmail.com>
2021-11-09 08:04:20 -0500

    moved animal functions to lib/animals.js file, starting to refactor server

---
+++ routes/apiRoutes/animalRoutes.js
@@ -0,0 +1,30 @@
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { animals } = require('../../data/animals');
+
+app.get("/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.get("/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.post("/animals", (req, res) => {
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
